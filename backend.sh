bb6291d6da5be6404dc272c645830e8a49a5dca0
RahulGattu912 <rahulgattu912@gmail.com>
2025-08-22 22:47:41 +0530

    cleared app directory if anything presents before unzipping

--- backend.sh
+++ backend.sh
@@ -73,7 +73,7 @@ cp /home/ec2-user/shell-project/backend.service /etc/systemd/system/backend.serv
 dnf install mysql -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h mysql.learndevops.online -u root -pExpenseApp@1 < /app/schema.sql &>>$LOG_FILE_NAME
+mysql -h mysql.learndevops.online -u root -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
 VALIDATE $? "Setting up the tranactions schema and tables"
 
 systemctl daemon-reload &>>$LOG_FILE_NAME
